3587d495ea8fa7b6bcc552d2c169ce13914c2158
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 17:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function yourAnimal() { return myAnimal(cat);
 }
 
 function add2(n) {
+   const two = 2
   return n + two
 
-  // Feel free to move things around!
-  const two = 2
+ 
 }
 
 var funkyFunction = function() {
